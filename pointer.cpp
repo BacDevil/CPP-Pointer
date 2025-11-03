7aab5865a09508468edb47857dff3fcb8301d3d5
I'm MAGIC <sonusundaray525@gmail.com>
2025-11-04 04:19:52 +0530

    Update pointer.cpp

--- pointer.cpp
+++ pointer.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  std::string box;
+std::string *ptr;
+std::cout << "enter your name: ";
+std::cin >> box;
+
+
+std::cout << "Do you want to modify this name?";
+std::cin >> box << std::endl;
+std::cout << "Modified name: " << box << std::endl;
+
+
+return 0;
+
+}
+
+
+
+
 
